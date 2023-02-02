ff3621be33f6ae96e4505160c6413add527bea1f
Namiyaly Azamat <ANamiyaly@beeline.kz>
2023-02-02 17:40:50 +0600

    init project and add element-plus ui library

---
+++ app.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <el-row class="mb-4">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
+    <el-button-group class="ml-4">
+      <el-button type="primary" :icon="Edit" />
+      <el-button type="primary" :icon="Share" />
+      <el-button type="primary" :icon="Delete" />
+    </el-button-group>
+
+    <el-button-group>
+      <el-button type="primary" :icon="ArrowLeft">Previous Page</el-button>
+      <el-button type="primary">
+        Next Page<el-icon class="el-icon--right"><ArrowRight /></el-icon>
+      </el-button>
+    </el-button-group>
+  </div>
+</template>
+<script setup lang="ts">
+import {
+  ArrowLeft,
+  ArrowRight,
+  Delete,
+  Edit,
+  Share,
+} from '@element-plus/icons-vue'
+</script>
